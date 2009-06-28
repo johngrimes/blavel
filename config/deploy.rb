0d923d8b66676c1b1395f5ff7c5a0139f1ba806a
John Grimes <John.Grimes@blavel.com>
2009-06-28 10:06:09 +0100

    Updating deployment for move to new server

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 set :application, "blavel"
 set :repository,  "git@github.com:johngrimes/blavel.git"
 set :scm, "git"
-set :deploy_to, "/home/webadmin/blavel.com"
+set :deploy_to, "/var/www/sites/blavel.com"
 
-set :user, "webadmin"
-set :runner, "webadmin"
+set :user, "deploy"
+set :runner, "deploy"
 
-role :app, "blavel.com"
-role :web, "blavel.com"
-role :db,  "blavel.com", :primary => true
+role :app, "67.23.31.183"
+role :web, "67.23.31.183"
+role :db,  "67.23.31.183", :primary => true
 
 namespace :deploy do
 
@@ -30,15 +30,15 @@ task :after_update_code, :roles => :app do
 
   # Create symbolic link to a common database.yml file in the system directory,
   # which is not under source control
-  run "ln -nfs #{shared_path}/system/db/database.yml #{release_path}/config/database.yml"
+  run "ln -nfs #{shared_path}/db/database.yml #{release_path}/config/database.yml"
 
   # Create symbolic link to a common environment.rb file in the system directory,
   # which is not under source control
-  run "ln -nfs #{shared_path}/system/config/environment.rb #{release_path}/config/environment.rb"
+  run "ln -nfs #{shared_path}/config/environment.rb #{release_path}/config/environment.rb"
 
   # Create symbolic links to common country and location data dumps
-  run "ln -nfs #{shared_path}/system/db/countries.txt #{release_path}/db/countries.txt"
-  run "ln -nfs #{shared_path}/system/db/locations.txt #{release_path}/db/locations.txt"
+  run "ln -nfs #{shared_path}/db/countries.txt #{release_path}/db/countries.txt"
+  run "ln -nfs #{shared_path}/db/locations.txt #{release_path}/db/locations.txt"
 
   # Generate documentation and link it to opencode in the public directory
   run "cd #{release_path}; rake doc:app"
@@ -46,12 +46,12 @@ task :after_update_code, :roles => :app do
 
   # Change the owner and group of everything under the deployment directory to
   # webadmin
-  sudo "chown -R webadmin:webadmin #{deploy_to}"
+  sudo "chown -R deploy:www-data #{deploy_to}"
 end
 
 task :after_setup, :roles => [:app, :db, :web] do
 
   # Change the owner and group of everything under the deployment directory to
   # webadmin
-  sudo "chown -R webadmin:webadmin #{deploy_to}"
+  sudo "chown -R deploy:www-data #{deploy_to}"
 end
