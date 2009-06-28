8d5012dbaa5037094a5981ecf37d3227bb454c1a
John Grimes <John.Grimes@blavel.com>
2009-06-28 08:39:40 +0100

    Installing new Gmail TLS plugin

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,9 +1,10 @@
 # Email settings
 ActionMailer::Base.smtp_settings = {
+  :tls => true,
   :address => "smtp.gmail.com",
   :port => 587,
   :domain => "blavel.com",
   :authentication => :plain,
-  :user_name => "feedback@blavel.com",
+  :user_name => "blavel@blavel.com",
   :password => "2001gattaca"
 }
