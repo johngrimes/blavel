96046cd07a74230b025d1f275070530bc44b78bc
John Grimes <John.Grimes@blavel.com>
2009-06-28 08:15:50 +0100

    Fixing mail to work with Gmail

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,14 +1,9 @@
 # Email settings
 ActionMailer::Base.smtp_settings = {
-  :address => 'mail.blavel.com',
-  :port => 25,
-  :domain => 'blavel.com',
-  :pop3_auth => { 
-    :server => 'mail.blavel.com', 
-    :user_name => 'blavel@blavel.com', 
-    :password => '$kav1nlambie',
-    :authentication => :login
-  }
+  :address => "smtp.gmail.com",
+  :port => 587,
+  :domain => "localhost",
+  :authentication => :plain,
+  :user_name => "blavel@blavel.com",
+  :password => "2001gattaca"
 }
-
-
