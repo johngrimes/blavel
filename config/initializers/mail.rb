25cca4eb73fb75e759ccce5137d91538de76822e
John Grimes <John.Grimes@blavel.com>
2009-06-28 08:22:06 +0100

    Changin domain on Gmail email settings

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,7 +2,7 @@
 ActionMailer::Base.smtp_settings = {
   :address => "smtp.gmail.com",
   :port => 587,
-  :domain => "localhost",
+  :domain => "blavel.com",
   :authentication => :plain,
   :user_name => "blavel@blavel.com",
   :password => "2001gattaca"
