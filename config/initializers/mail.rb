680cd5aad463b0a554988610a90786cf95339831
John Grimes <john@smallspark.com.au>
2011-06-24 08:11:06 +1000

    Changing password for feedback@blavel.com

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -6,5 +6,5 @@ ActionMailer::Base.smtp_settings = {
   :domain => "blavel.com",
   :authentication => :plain,
   :user_name => "feedback@blavel.com",
-  :password => "2001gattaca"
+  :password => "Rambl1nman"
 }
